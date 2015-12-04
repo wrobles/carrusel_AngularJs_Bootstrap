730ade1d680b7a30ca8cacdc290e5429e942373c
wrobles <wen.robles@gmail.com>
2015-12-04 13:04:31 -0800

    04/Dic/2015 - Version 1.0.0 - Estable pero no optimizada

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name carruselApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the carruselApp
+ */
+angular.module('carruselApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    /*this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];*/
+    var baseURL='http://lorempixel.com/960/450/';
+    $scope.setInterval = 5000; 
+    $scope.noWrapSlides = false;
+    $scope.slides = [
+		{
+			title:'Aprende a mantenerte en forma',
+			image: baseURL+'sports/',
+			text:'¡Practica algún deporte todos los días!'
+		},
+		{
+			title:'Buena alimentación',
+			image: baseURL+'food/',
+			text:'¡La importancia de frutas y verduras en tu dieta diaria!'
+		},
+		{
+			title:'El contacto con la naturaleza',
+			image: baseURL+'nature/',
+			text:'¡Mantente en armonía con la naturaleza!'
+		}
+    ];
+  }]);
